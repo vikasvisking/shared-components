7feb7f5cfaf5939fdb55eb2d41a618f96bd3b719
Vikas <vikasvisking007@gmail.com>
2021-08-05 21:10:29 +0530

    Add components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import TodaysDate from './Test';
-import  Todays2Date from './Test2';
-import  Today3 from './Test3';
+export { default as SearchNotFound } from './components/SearchNotFound';
+export { default as SvgIconStyle } from './components/SvgIconStyle';
+export { default as ScrollToTop } from './components/ScrollToTop';
+export { default as Scrollbar } from './components/Scrollbar';
+export { default as Page } from './components/Page';
+export { default as NavSection } from './components/NavSection';
+export { default as MenuPopover } from './components/MenuPopover';
+export { default as Logo } from './components/Logo';
+export { default as Label } from './components/Label';
+export { default as ColorPreview } from './components/ColorPreview';
+export { default as ColorManyPicker } from './components/ColorManyPicker';
+export { default as DashboardApp } from './components/_dashboard/app';
+export { default as MHidden } from './components/@material-extend';
 
-export default TodaysDate;
-export {  Todays2Date, Today3 }
+
+
+
+
+
+
+
+
+
